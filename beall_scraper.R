7f373d10fffe5ae91b86ac9b6c0ec9cfebdb741f
rewberl <rewberl@gmail.com>
2017-01-26 16:34:25 -0700

    Update scraper

--- beall_scraper.R
+++ beall_scraper.R
@@ -10,12 +10,14 @@
 # Load packages
 library(rvest)
 library(selectr)
+library(stringr)
 
 # Read pages from disk
 publishers = read_html("./raw/publishers/index.html")
 journals = read_html("./raw/journals/index.html")
 hijacked = read_html("./raw/hijacked/index.html")
 metrics = read_html("./raw/metrics/index.html")
+
 
 # Scrape information from publishers page
 ## Uses CSS selectors obtained using SelectorGadget:
@@ -26,7 +28,7 @@ p = html_nodes(publishers, ".body div div div+ ul li")
 p.text = html_text(p)
 p.text = substr(p.text, 3, length(p.text))
 
-## Scrape links to list (since number of links per text entry varies) and clean
+## Scrape links to list (since some text entries have two links) and clean
 p.links = list()
 for (i in 1:length(p)) {
     temp1 = html_attr(html_nodes(p[i], "a"), "href")
@@ -38,20 +40,52 @@ for (i in 1:length(p)) {
     }
 }
 
-### Add links written out in text (e.g. #266) by searching for domain extensions
-### Probably concatenate links to one column
+### Add non-hyperlinked sites from inside text entries
+p.links[[266]] = append(p.links[[266]], "http://shankargargh.org/")
 
-## Extract alternate names from text
+### Split links to two variables
+p.links.1 = rep(NA, length(p.links))
+p.links.2 = rep(NA, length(p.links))
+for (i in 1:length(p.links)) {
+    if (!is.na(p.links[[i]][1])) {
+        p.links.1[i] = p.links[[i]][1]
+        if (length(p.links[[i]]) == 2) {
+            p.links.2[i] = p.links[[i]][2]
+        }
+    }
+}
+
+## Extract alternate names from text and remove from text
 for (i in 1:length(p.text)) {
     temp = c()
-    if (grepl("(", p.text[i], fixed=T)) {
-        temp = append(temp, regmatches(p.text[i],
-                                       gregexpr("(?<=\\().*?(?=\\))",
-                                                p.text[i], perl=T))[[1]])
+    if (str_detect(p.text[i], "\\(")) {
+        temp = append(temp,
+                      str_extract(p.text[i], "(?<=\\().*?(?=\\))"))
     }
-    if (grepl("see", p.text[i], fixed=T, ignore.case=T)) {
-#        temp = append(temp, )
+    if (str_detect(p.text[i], "\\/")) {
+        temp = append(temp,
+                      str_extract(p.text[i], "([^/]+$)"))
     }
+    if (str_detect(p.text[i], fixed("see", ignore_case=T))) {
+        temp = append(temp,
+                      tail(unlist(str_split(p.text[i],
+                                            fixed("see",
+                                                  ignore_case=T))), 1))
+    }
+    if (str_detect(p.text[i], fixed("also called", ignore_case=T))) {
+        temp = append(temp,
+                      tail(unlist(str_split(p.text[i],
+                                            fixed("also called",
+                                                  ignore_case=T))), 1))
+    }
+    if (str_detect(p.text[i], fixed("re-branded as", ignore_case=T))) {
+        temp = append(temp,
+                      tail(unlist(str_split(p.text[i],
+                                            fixed("re-branded as",
+                                                  ignore_case=T))), 1))
+    }
+    # From alternate names: split by ";", "|", ",", ":", remove junk characters, split by white space
+    # From text: remove all content matched above, then characters used for matching
 }
 
 
